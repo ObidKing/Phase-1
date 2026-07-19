b4e4cf11a6c89849d5cad571e174e123c6f540c7
dee <obiidemudia@gmail.com>
2026-07-19 07:21:37 -0400

    day 1 completed and first commit

---
+++ Phase1/wk1/Day1/Exercises/menu.py
@@ -0,0 +1,24 @@
+#breakfast menu
+
+hs ="Hickory-Smoked Bacon Stripe"
+hb = "Hash Browns"
+fj = "Fruit Juice"
+bs = "Breakfast Sampler"
+bfb ="Big FramerHouse BreakFEAST"
+dcp ="Dubai Chocolate Pancake"
+c = "Coffee"
+it = "Ice Tea"
+
+
+print("Welcome for Breakfast!!!")
+print("These is what we have for BreakFast Today")
+
+print(hs, hb, fj, bs, bfb, dcp, c, it , sep="\n")
+
+# Intentionally create a syntax error.
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+#     print(hs, hb, fj, bs, bfb, dcp, c, it , sep="\n")
+#           ^^
+# NameError: name 'hs' is not defined
+#hs is not defined before use
---
+++ Phase1/wk1/Day1/Exercises/name.py
@@ -0,0 +1,8 @@
+print("Hello, Python")
+print("dee ")
+
+
+a = "this is home"
+b = "learn llm"
+c = "cuda what"
+print(a,b,c, sep='\n')
